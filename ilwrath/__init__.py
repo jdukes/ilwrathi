aae6e69bd6bfff8bd9baa002cf9a943c2e8f8f63
hex <hex@neg9.org>
2014-09-20 20:31:58 -0700

    fixed everything after the move, verified demo works

--- ilwrath/__init__.py
+++ ilwrath/__init__.py
@@ -5,4 +5,9 @@ Ilwrath currently contains Sac. Sac is a base class that can be
 extended to handle performing operations that have multiple
 dependances. This was created to solve the problem of testing web
 services with multiple dependances."""
+from sys import version_info as _version_info
 
+if _version_info.major == 2:
+    from ._py2 import Sac
+
+__all__ = ["Sac"]
--- ilwrath/_py2/__init__.py
+++ ilwrath/_py2/__init__.py
@@ -1,2 +1,4 @@
 #!/usr/bin/env python2
 from .sac import Sac
+
+__all__ = ["Sac"]
