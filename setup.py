1280ca938a5fa78a84b2d23de0a570e237ae77d9
hex <hex@neg9.org>
2014-09-20 20:25:23 -0700

    initial setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+import os
+import sys
+sys.path.insert(0,os.path.join(os.path.dirname(__file__),'ilwrath'))
+import ilwrath
+
+setup(name="ilwrath",
+      version="0.1",
+      description="A framework for building web pen test tools",
+      url="https://github.com/jdukes/ilwrath",
+      author="Josh Dukes",
+      author_email="hex@neg9.org",
+      license="GNU General Public License v3 (GPLv3)",
+      keywords = "hacker, web, CTF, pen test",
+      long_description=ilwrath.__doc__,
+      packages=["ilwrath"])
