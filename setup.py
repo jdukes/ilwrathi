4406ad1dc54d027e4b26e6056ec8cdedb809c013
Josh Dukes <hex@neg9.org>
2014-10-04 14:30:16 -0700

    naming scheme first rev change

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 from setuptools import setup
+from datetime import datetime #for version string
 
 import os
 import sys
 sys.path.insert(0,os.path.join(os.path.dirname(__file__),'ilwrath'))
 import ilwrath
 
-setup(name="ilwrath",
-      version="0.3",
-      description="A framework for building web pen test tools",
+now = datetime.now()
+
+setup(name="ilwrathi",
+      version="%s%s.1a3" % (now.year, now.month), # PEP440 compliant
+      # The first section lets users know how old a module is, the
+      # second lets the user compare relative versions of the same age.
+      description="A framework for building pen test tools",
       url="https://github.com/jdukes/ilwrath",
       author="Josh Dukes",
       author_email="hex@neg9.org",
