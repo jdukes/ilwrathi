a661d191de5d62c91bca7343dabd52cade73ce59
Josh Dukes <joshdukes@Joshs-MacBook-Air.local>
2014-10-07 09:28:14 -0700

    fixed test naming

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,5 @@
 #/usr/bin/env python
+#TODO: add doctests to test suite
 from sys import version_info as _version_info
 
-from .test_ilwrathi__common_iac import TestIdempotentAccessor
-
-#add doctests??
+from .test_ilwrathi_iac import TestIdempotentAccessor
